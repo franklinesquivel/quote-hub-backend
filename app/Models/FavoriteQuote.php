84c03c05ae37de8f5e44e887a3aeb08e893b80db
Franklin Esquivel <franklin.esquivel@outlook.com>
2023-05-28 22:11:06 -0600

    feat(quotes): Adds mixin to eloquent model

--- app/Models/FavoriteQuote.php
+++ app/Models/FavoriteQuote.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @mixin IdeHelperFavoriteQuote
+ */
 class FavoriteQuote extends Model
 {
     use UsesUuid;
