8637276de884e63c9a345a74357f630b6721ebb7
guerretta16 <guerret98@gmail.com>
2023-05-30 16:01:08 -0600

    feat(constants): Quote constants types added.
    - Constants for `quote proposals`.
    - Constants for `quote`.

---
+++ app/Constants/QuoteProposalTypesConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+final class QuoteProposalTypesConstant
+{
+    public const PENDING = 'PENDING';
+
+    public const ACCEPTED = 'ACCEPTED';
+
+    public const REJECTED = 'REJECTED';
+
+}
