a873f6f24a76156a0a63a1330d722164f8c81eb9
guerretta16 <guerret98@gmail.com>
2023-05-30 16:06:01 -0600

    feat(request): ReviewQuoteProposalRequest created.
    Form request for review the proposals.

---
+++ app/Http/Requests/QuoteProposals/ReviewQuoteProposalRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\QuoteProposals;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewQuoteProposalRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'rejected_reason' => 'nullable|string',
+            'status' => 'required|string',
+        ];
+    }
+}
