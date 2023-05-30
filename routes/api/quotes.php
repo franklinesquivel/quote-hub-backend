5741ab69b24b2057133957b193017a8befa2a232
guerretta16 <guerret98@gmail.com>
2023-05-30 09:45:18 -0600

    refactor(proposal): Refactor for Get all pending for aprproval proposal controller.

--- routes/api/quotes.php
+++ routes/api/quotes.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\Quotes\CreateQuoteController;
 use App\Http\Controllers\Quotes\GetUserFavoriteQuotesController;
 use App\Http\Controllers\Quotes\Proposals\CreateQuoteProposalController;
-use App\Http\Controllers\Quotes\Proposals\GetAllQuoteProposalsController;
+use App\Http\Controllers\Quotes\Proposals\GetAllPendingForApprovalProposalsController;
 use App\Http\Controllers\Quotes\Proposals\ReviewQuoteProposalController;
 
 Route::post('/', CreateQuoteController::class);
 Route::get('/favorite', GetUserFavoriteQuotesController::class);
-Route::get('/proposal', GetAllQuoteProposalsController::class);
+Route::get('/proposal', GetAllPendingForApprovalProposalsController::class);
 Route::post('/proposal', CreateQuoteProposalController::class);
 Route::patch('/proposal/{quote_proposal}', ReviewQuoteProposalController::class);
