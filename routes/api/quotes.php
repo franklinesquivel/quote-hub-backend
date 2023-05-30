f3b400ac0345c903dc084bf691b065869f1cc9fb
guerretta16 <guerret98@gmail.com>
2023-05-30 16:16:46 -0600

    feat(routes): Routes for Quote Proposal added.
    - `Get all pending for approval proposal` [Admin ability].
    - `Create quote proposal` [User ability].
    - `Review quote proposal` [Admin ability].
    
    All routes have their middlewares in the controller's constructor, with their abilities too.

--- routes/api/quotes.php
+++ routes/api/quotes.php
@@ -2,6 +2,12 @@
 
 use App\Http\Controllers\Quotes\CreateQuoteController;
 use App\Http\Controllers\Quotes\GetUserFavoriteQuotesController;
+use App\Http\Controllers\Quotes\Proposals\CreateQuoteProposalController;
+use App\Http\Controllers\Quotes\Proposals\GetAllPendingForApprovalProposalsController;
+use App\Http\Controllers\Quotes\Proposals\ReviewQuoteProposalController;
 
 Route::post('/', CreateQuoteController::class);
 Route::get('/favorite', GetUserFavoriteQuotesController::class);
+Route::get('/proposal', GetAllPendingForApprovalProposalsController::class);
+Route::post('/proposal', CreateQuoteProposalController::class);
+Route::patch('/proposal/{quote_proposal}', ReviewQuoteProposalController::class);
