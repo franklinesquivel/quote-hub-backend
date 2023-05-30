f37d8ec7283bb14ffe8dc7e118e94670e9447bd4
guerretta16 <guerret98@gmail.com>
2023-05-30 14:07:06 -0600

    refactor(models): Command `php artisan ide-helper:models -M` executed.
    This adds `mixin` for `QuoteProposal` and updates `Quote` models.

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
- * @mixin IdeHelperFavoriteQuote
+ * @mixin IdeHelperQuote
  */
 class Quote extends Model
 {
