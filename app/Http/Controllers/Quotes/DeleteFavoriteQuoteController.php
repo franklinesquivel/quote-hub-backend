3f84a54abb3c55f78821d26641f2145ff2b49c3a
Eduardo Guerretta <91360573+guerretta16@users.noreply.github.com>
2023-05-31 14:58:54 -0600

    Feature/Delete favorite quotes (#14)
    Closes #8

---
+++ app/Http/Controllers/Quotes/DeleteFavoriteQuoteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Quotes;
+
+use App\Constants\QuoteTypesConstant;
+use App\Constants\UserTypesAbilitiesConstant;
+use App\Http\Controllers\Controller;
+use App\Models\Quote;
+use Illuminate\Http\JsonResponse;
+
+class DeleteFavoriteQuoteController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware(['auth:sanctum', 'abilities:' . UserTypesAbilitiesConstant::USER]);
+    }
+
+    public function __invoke(Quote $quote): JsonResponse
+    {
+        if ($quote->type !== QuoteTypesConstant::FAVORITE) {
+            abort(409);
+        }
+
+        if ($quote->user->id !== auth()->id()) {
+            abort(403);
+        }
+
+        $quote->delete();
+        return response()->json([], 204);
+    }
+}
--- routes/api/quotes.php
+++ routes/api/quotes.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\Quotes\CreateQuoteController;
+use App\Http\Controllers\Quotes\DeleteFavoriteQuoteController;
 use App\Http\Controllers\Quotes\GetUserFavoriteQuotesController;
 use App\Http\Controllers\Quotes\Proposals\CreateQuoteProposalController;
 use App\Http\Controllers\Quotes\Proposals\GetAllPendingForApprovalProposalsController;
 use App\Http\Controllers\Quotes\Proposals\ReviewQuoteProposalController;
 
 Route::post('/', CreateQuoteController::class);
+Route::delete('/{quote}', DeleteFavoriteQuoteController::class);
 Route::get('/favorite', GetUserFavoriteQuotesController::class);
 Route::get('/proposal', GetAllPendingForApprovalProposalsController::class);
 Route::post('/proposal', CreateQuoteProposalController::class);
