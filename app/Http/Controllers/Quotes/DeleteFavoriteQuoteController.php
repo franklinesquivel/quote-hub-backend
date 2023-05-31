b7940aeaf609c9b4f21333056fee57f5803217ab
guerretta16 <guerret98@gmail.com>
2023-05-31 08:54:51 -0600

    feature(controller): `DeleteFavoriteQuoteController` created.
    PHPDocs comments were deleted from the controller.

---
+++ app/Http/Controllers/Quotes/DeleteFavoriteQuoteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Quotes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeleteFavoriteQuoteController extends Controller
+{
+
+    public function __invoke(Request $request)
+    {
+
+    }
+}
