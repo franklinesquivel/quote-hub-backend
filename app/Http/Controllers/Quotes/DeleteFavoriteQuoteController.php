e05c92f19e2a6a2b4e6632c58eab4866359845ee
guerretta16 <guerret98@gmail.com>
2023-05-31 09:37:40 -0600

    feature(controller): Restrictions for `DeleteFavoriteController` was added and delete function.
    - `Conflict` message with status 409 was added.
    - `Forbidden` message with status 403 was added.
    - Status `204` added if it's ok!

--- app/Http/Controllers/Quotes/DeleteFavoriteQuoteController.php
+++ app/Http/Controllers/Quotes/DeleteFavoriteQuoteController.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Controllers\Quotes;
 
+use App\Constants\QuoteTypesConstant;
+use App\Constants\UserTypesAbilitiesConstant;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Quote;
+use Illuminate\Http\JsonResponse;
 
 class DeleteFavoriteQuoteController extends Controller
 {
 
-    public function __invoke(Request $request)
+    public function __construct()
     {
+        $this->middleware(['auth:sanctum', 'abilities:' . UserTypesAbilitiesConstant::USER]);
+    }
 
+    public function __invoke(Quote $quote): JsonResponse
+    {
+        if ($quote->type !== QuoteTypesConstant::FAVORITE) {
+            return response()->json('Conflict', 409);
+        }
+
+        if ($quote->user->id !== auth()->id()) {
+            return response()->json('Forbidden', 403);
+        }
+
+        $quote->delete();
+        return response()->json([], 204);
     }
 }
